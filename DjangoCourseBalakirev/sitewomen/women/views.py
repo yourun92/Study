ca3bbbeff7d295b9d9995797956e62c2fe77a24a
yourun92 <ostanin.iurii@gmail.com>
2025-06-04 17:37:17 +0300

    lets go

--- DjangoCourseBalakirev/sitewomen/women/views.py
+++ DjangoCourseBalakirev/sitewomen/women/views.py
@@ -5,24 +5,20 @@ from django.urls import reverse
 from django.template.loader import render_to_string
 
 # Create your views here.
-menu = ['О сайте', 'Обо мне', 'Категории', 'Архив']
+menu = ['О сайте', 'Добавить статью', 'Обратная связь', 'Войти']
 
-class MyClass:
-	def __init__(self, a, b):
-		self.a = a
-		self.b = b
+data_db = [ {'id': 1, 'title': 'Анджелина Джоли', 'content': 'Биография Анджелины Джоли', 'is_published': True},
+            {'id': 2, 'title': 'Марго Робби', 'content': 'Биография Марго Робби', 'is_published': False},
+            {'id': 3, 'title': 'Джулия Робертс', 'content': 'Биография Джулии Робертс', 'is_published': True}, ]
+
 
 
 def index(request):
 	data = {
-		'title': 'главная страница',
+		'title': 'Главная страница',
 		'menu': menu,
-		'float': 27.64,
-		'lst': [1, 2, 'fdc', True],
-		'set': {1, 2, 3, 4, 5},
-		'dict': {'key_1': 'value_1', 'key_2': 'value_2'},
-		'obj': MyClass(10, 20)
-	}
+		'posts': data_db
+		}
 	return render(request, 'women/index.html', context=data)
 
 def about(request):
